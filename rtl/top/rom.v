1dd995ca70d3d3d214abdcfa13171f99d0afe5f7
keitodesu <g2121057@tcu.ac.jp>
2024-09-30 14:25:00 +0900

    chage 16

--- rtl/top/rom.v
+++ rtl/top/rom.v
@@ -29,18 +29,18 @@ end*/
 	//Sn+1 = sn + n
 	always @(*) begin
     case (adrs)
-        8'h00 : rom_data = 8'b00000001; // LDI
-        8'h01 : rom_data = 8'h00;       // 0
-        8'h02 : rom_data = 8'b00000101; // ST
-        8'h03 : rom_data = 8'b00100001; // Sn#21 = 0
-        8'h04 : rom_data = 8'b00000001; // LDI
-        8'h05 : rom_data = 8'b00000001; // 1
-        8'h06 : rom_data = 8'b00000101; // ST
-        8'h07 : rom_data = 8'b00100000; // n#20 <= 1
-        8'h08 : rom_data = 8'b00000100; // ADD
-        8'h09 : rom_data = 8'b00100001; // GR <= Sn#21 + 1
-        8'h0A : rom_data = 8'b00000101; // ST
-        8'h0B : rom_data = 8'b00100001; // Sn#21 <= GR
+        8'h00 : rom_data = 8'b00000001; // LDI  //01
+        8'h01 : rom_data = 8'h00;       // 0   
+        8'h02 : rom_data = 8'b00000101; // ST  //05
+        8'h03 : rom_data = 8'b00100001; // Sn#21 = 0 //21
+        8'h04 : rom_data = 8'b00000001; // LDI //01
+        8'h05 : rom_data = 8'b00000001; // 1   //1
+        8'h06 : rom_data = 8'b00000101; // ST //05
+        8'h07 : rom_data = 8'b00100000; // GR #20 <= 1 //20
+        8'h08 : rom_data = 8'b00000100; // ADD   //04
+        8'h09 : rom_data = 8'b00100001; // GR <= Sn#21 + 1 //21
+        8'h0A : rom_data = 8'b00000101; // ST //05
+        8'h0B : rom_data = 8'b00100001; // Sn#21 <= GR 
         8'h0C : rom_data = 8'b00000010; // LD
         8'h0D : rom_data = 8'b00100000; // GR <= n#20
         8'h0E : rom_data = 8'b00000011; // ADDI
